316e93d2ce2c8ba44a5bfa21f42ad4b9f6a3b01f
Alexander Evseev <skitonline@yandex.ru>
2026-08-10 11:20:46 +0400

    add pytets.ini

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,7 @@
 from pages.login_page import LoginPage
 
 
-def test_login_fail(driver, base_url):
+def test_login_succes(driver, base_url):
     page = LoginPage(driver)
     page.open(base_url)
     page.login("user", "password")
